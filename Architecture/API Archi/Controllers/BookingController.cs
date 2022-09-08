3b4dedce0292ba91d98974d1841f0c349f7cd675
Khetamaru <lasserre@intechinfo.fr>
2022-09-08 16:27:15 +0200

    Step 2

---
+++ Architecture/API Archi/Controllers/BookingController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace API_Archi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookingController : ControllerBase
+    {
+        private readonly ILogger<BookingController> _logger;
+
+        public BookingController(ILogger<BookingController> logger)
+        {
+            _logger = logger;
+        }
+
+        // http://localhost:52880/Booking/
+        [HttpGet]
+        public IEnumerable<Booking> Get()
+        {
+            string fileName = "Booking.json";
+            string jsonString = System.IO.File.ReadAllText(fileName);
+            return JsonSerializer.Deserialize<Booking[]>(jsonString);
+        }
+
+        // http://localhost:52880/Booking/
+        [HttpPost]
+        public void Post(Booking booking)
+        {
+            string fileName = "Booking.json";
+            string jsonString = JsonSerializer.Serialize(booking);
+            System.IO.File.WriteAllText(fileName, jsonString);
+        }
+
+        // http://localhost:52880/Booking/checkFlightLimit/{id}/{date}
+        [HttpGet("checkFlightLimit/{id}/{date}")]
+        public bool CheckFlightLimit(int id, DateTime date)
+        {
+            string fileName = "Booking.json";
+            string jsonString = System.IO.File.ReadAllText(fileName);
+            Booking[] bookings = JsonSerializer.Deserialize<Booking[]>(jsonString);
+
+            Flight[] flights = FlightController.FlightTab;
+            int limit = 0;
+
+            foreach(Flight flight in flights)
+            {
+                if (flight.id == id) limit = flight.nbMaxPlaces;
+            }
+
+            int limitTrigger = 0;
+
+            foreach(Booking booking in bookings)
+            {
+                if (booking.FlightId == id && booking.date == date) 
+                {
+                    limitTrigger++;
+                }
+            }
+
+            if (limitTrigger < limit)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Architecture/API Archi/Controllers/FlightController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Archi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FlightController : ControllerBase
+    {
+        public static readonly Flight[] FlightTab = new Flight[]
+        {
+            new Flight(0, "CDG", "JFK", 1000, 750),
+            new Flight(1, "JFK", "CDG", 1000, 750),
+            new Flight(2, "CDG", "DTW", 700, 500),
+            new Flight(3, "DTW", "CDG", 700, 500),
+            new Flight(4, "JFK", "DTW", 300, 250),
+            new Flight(5, "DTW", "JFK", 300, 250)
+        };
+
+        private readonly ILogger<FlightController> _logger;
+
+        public FlightController(ILogger<FlightController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Flight> Get()
+        {
+            return FlightTab;
+        }
+    }
+}
