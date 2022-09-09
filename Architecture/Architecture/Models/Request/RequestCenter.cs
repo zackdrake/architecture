f667a2640096a1a693e4d974b6eb3af4a26e0661
Khetamaru <lasserre@intechinfo.fr>
2022-09-09 14:54:28 +0200

    Bux Fig

--- Architecture/Architecture/Models/Request/RequestCenter.cs
+++ Architecture/Architecture/Models/Request/RequestCenter.cs
@@ -8,7 +8,7 @@ namespace Architecture.Models.Request
     public class RequestCenter
     {
         public static List<Flight> GetFlights() => JsonSerializer.Deserialize<List<Flight>>(RequestLauncher.LaunchRequest(RequestLauncher.METHOD.GET, RequestLauncher.CONTROLLER.Flight, string.Empty));
-        public static List<Flight> GetDiscounts(int flightId) => JsonSerializer.Deserialize<List<Flight>>(RequestLauncher.LaunchRequest(RequestLauncher.METHOD.GET, RequestLauncher.CONTROLLER.Flight, flightId.ToString()));
+        public static List<Flight> GetPrice(int flightId, bool discount, bool option) => JsonSerializer.Deserialize<List<Flight>>(RequestLauncher.LaunchRequest(RequestLauncher.METHOD.GET, RequestLauncher.CONTROLLER.Flight, flightId.ToString() + "/" + discount + "/" + option));
         public static List<Booking> PostBooking(Booking booking) => JsonSerializer.Deserialize<List<Booking>>(RequestLauncher.LaunchRequest(RequestLauncher.METHOD.POST, RequestLauncher.CONTROLLER.Booking, string.Empty, JsonSerializer.Serialize(booking)));
         public static bool CheckFlightLimit(int flightId, DateTime date) => JsonSerializer.Deserialize<bool>(RequestLauncher.LaunchRequest(RequestLauncher.METHOD.GET, RequestLauncher.CONTROLLER.Booking, flightId.ToString() + "/" + date.ToString("dd'-'mm'-'YY'T'HH':'mm':'ss")));
     }
