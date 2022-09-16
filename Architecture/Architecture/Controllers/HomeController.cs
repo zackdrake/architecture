669db1c13973a18c0b0844aac9c3cbc4259e69b8
Dracosinus <bus26@sfr.fr>
2022-09-16 16:52:55 +0200

    WIP display tickets on SPA

--- Architecture/Architecture/Controllers/HomeController.cs
+++ Architecture/Architecture/Controllers/HomeController.cs
@@ -13,47 +13,58 @@ namespace Architecture.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private List<Booking> _Bookings;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(List<Booking> bookings = null)
         {
             var model = new VolsViewModel(RequestCenter.GetFlights());
+            if (bookings != null){
+                model.Bookings = bookings;
+            }
             return View(model);
         }
 
         [HttpPost]
-        public IActionResult IndexPost(int flight, bool Child, bool Bagage, string FirstName, string LastName, DateTime Date, string submit)
+        public IActionResult IndexPost(int flight, bool Child, bool Luggage, string FirstName, string LastName, DateTime Date, List<Booking> bookings, string submit)
         {
-            if (submit == "transaction")
-            {
-                var booking = new Booking(0, FirstName, LastName, flight, Date);
-                if (_Bookings == null)
-                    _Bookings = new List<Booking>();
-                _Bookings.Add(booking);
-                return Transaction();
+            Booking booking = new Booking(0, FirstName, LastName, flight, Date);
+            if (bookings == null) bookings = new List<Booking>();
+            bookings.Add(booking);            
+            if (submit == "transaction"){
+                return Index(bookings);
             }
-            else
-            {
-                var booking = new Booking(0, FirstName, LastName, flight, Date);
-                Booking bookingTest = RequestCenter.PostBooking(booking);
+            else {
                 return Redirect("Index");
             }
+            // if (submit == "transaction")
+            // {
+            //     var booking = new Booking(0, FirstName, LastName, flight, Date);
+            //     if (_Bookings == null)
+            //         _Bookings = new List<Booking>();
+            //     _Bookings.Add(booking);
+            //     return Transaction();
+            // }
+            // else
+            // {
+            //     var booking = new Booking(0, FirstName, LastName, flight, Date);
+            //     Booking bookingTest = RequestCenter.PostBooking(booking);
+            //     return Redirect("Index");
+            // }
         }
 
-        public IActionResult Transaction()
-        {
-            if(_Bookings == null)
-            {
-                _Bookings = new List<Booking>();
-            }
-            var model = new VolsViewModel(_Bookings);
-            return View("Transaction",model);
-        }
+        // public IActionResult Transaction()
+        // {
+        //     if(bookings == null)
+        //     {
+        //         bookings = new List<Booking>();
+        //     }
+        //     var model = new VolsViewModel(bookings);
+        //     return View("Transaction",model);
+        // }
 
         public IActionResult Privacy()
         {
