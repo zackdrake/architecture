553b5f3f820a97a572699620442b145f1f619294
BenjaminPayet <benjamin.payet@esiea.fr>
2022-09-26 16:11:10 +0200

    site.js now get all the informations of flight including source and train

--- Architecture/Architecture/wwwroot/js/site.js
+++ Architecture/Architecture/wwwroot/js/site.js
@@ -5,10 +5,102 @@ function funcPayer() {
 function funcTransac() {
     document.getElementById('submit').value = 'transaction';
 }
-function SourceOption(source) {
+function SourceOption(flight) {
     console.log("test")
+    console.log(flight.id)
     let option = document.getElementById('options');
-    option.innerHTML = '<div><label for="Child">Tariffeeeeee enfant -10ans => -10%</label><input type="checkbox" name="Child" checked value="true" <input type="hidden" name="Child" value="false" /></div >'
+    if (source == intern) {
+        option.innerHTML = `
+            <div name="InternalFlightDiv">
+            
+
+
+
+            <div name="ChildDiv">
+                <label for="Child">Tarif enfant -10ans => -10%</label>
+                <input type="checkbox" name="Child" checked value="true" />
+                <input type="hidden" name="Child" value="false" />
+            </div>
+            <div name="LuggageDiv">
+                <label for="Luggage">Bagage : 50€</label>
+                <input type="checkbox" name="Luggage" checked value="true" />
+                <input type="hidden" name="Luggage" value="false" />
+            </div>
+
+        </div>
+`
+    }
+    if (source == external) {
+        option.innerHTML = `
+        <div name="ExternalFlightDiv">
+            
+
+
+
+            <div name="ChildDiv">
+                <label for="Child">Tarif enfant -10ans => -10%</label>
+                <input type="checkbox" name="Child" checked value="true" />
+                <input type="hidden" name="Child" value="false" />
+            </div>
+            <div name="LuggageDiv">
+                <label for="Luggage">Bagage : 50€</label>
+                <input type="checkbox" name="Luggage" checked value="true" />
+                <input type="hidden" name="Luggage" value="false" />
+            </div>
+            <div name="FirstClassDiv">
+                <label for="FirstClass">Première Classe : 50€</label>
+                <input type="checkbox" name="FirstClass" checked value="true" />
+                <input type="hidden" name="FirstClass" value="false" />
+            </div>
+            <div name="ChampagneOnBoardDiv">
+                <label for="ChampagneOnBoard">Champagne : ??€</label>
+                <input type="checkbox" name="ChampagneOnBoard" checked value="true" />
+                <input type="hidden" name="ChampagneOnBoard" value="false" />
+            </div>
+            <div name="LoungAccessDiv">
+                <label for="LoungeAccess">Acces Lounge : ??€</label>
+                <input type="checkbox" name="LoungeAccess" checked value="true" />
+                <input type="hidden" name="LoungeAccess" value="false" />
+            </div>
+        </div >
+`
+    }
+    if (source == broker) {
+        option.innerHTML = `
+        <div name="ExternalFlightDiv">
+            
+
+
+
+            <div name="ChildDiv">
+                <label for="Child">Tarif enfant -10ans => -10%</label>
+                <input type="checkbox" name="Child" checked value="true" />
+                <input type="hidden" name="Child" value="false" />
+            </div>
+            <div name="LuggageDiv">
+                <label for="Luggage">Bagage : 50€</label>
+                <input type="checkbox" name="Luggage" checked value="true" />
+                <input type="hidden" name="Luggage" value="false" />
+            </div>
+            <div name="FirstClassDiv">
+                <label for="FirstClass">Première Classe : 50€</label>
+                <input type="checkbox" name="FirstClass" checked value="true" />
+                <input type="hidden" name="FirstClass" value="false" />
+            </div>
+            <div name="ChampagneOnBoardDiv">
+                <label for="ChampagneOnBoard">Champagne : ??€</label>
+                <input type="checkbox" name="ChampagneOnBoard" checked value="true" />
+                <input type="hidden" name="ChampagneOnBoard" value="false" />
+            </div>
+            <div name="LoungAccessDiv">
+                <label for="LoungeAccess">Acces Lounge : ??€</label>
+                <input type="checkbox" name="LoungeAccess" checked value="true" />
+                <input type="hidden" name="LoungeAccess" value="false" />
+            </div>
+        </div >
+            `
+    }
+
 }
 
 
