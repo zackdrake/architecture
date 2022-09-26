a57b903e83ae99015e1ea80883a6f48014a244b7
Dracosinus <bus26@sfr.fr>
2022-09-26 10:21:01 +0200

    Broker data model

---
+++ Architecture/API Archi/BrokerBooking.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace API_Archi
+{
+    public class BrokerBooking
+    {
+        public int total_price { get; set; }
+
+        public BrokerCurrency currency { get; set; }
+
+        public BrokerTickets[] tickets { get; set; }
+
+        public BrokerBooking(int total_price, BrokerCurrency currency, BrokerTicket[] tickets)
+        {
+            this.total_price = total_price;
+            this.currency = currency;
+            this.tickets = tickets;
+        }
+    }
+}
---
+++ Architecture/API Archi/BrokerTicket.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace API_Archi
+{
+    public class BrokerTicket
+    {
+        public string flight_code { get; set; }
+
+        public BrokerPerson person { get; set; }
+
+        public int price { get; set; }
+
+        public string booking_date { get; set; } //"23/09/2022"
+
+        public BrokerAvailableOptions[] options_codes { get; set; }
+
+        public BrokerTicket(string flight_code, BrokerPerson person, int price, string booking_date, BrokerAvailableOptions[] option_codes)
+        {
+            this.flight_code = flight_code;
+            this.person = person;
+            this.price = price;
+            this.booking_date = booking_date;
+            this.option_codes = option_codes;
+        }
+    }
+}
